b4bc5c6ea32a486d6db09c6ceb8ffc1d11a398ce
Tobias Köhler <tobiaskohler@Mac.fritz.box>
2025-03-08 11:30:33 +0100

    add unit test

--- src/app/models/diary.slice.ts
+++ src/app/models/diary.slice.ts
@@ -8,14 +8,5 @@ export interface DiarySlice {
 export const initialDiaryState: DiarySlice = {
   selectedId: undefined,
   diaryEntries: [
-    {
-      date: "2025-03-05",
-      id: "1",
-      stomach: 7,
-      sleepQuality: 2,
-      food: ["food"],
-      drinks: ["drinks"],
-      medication: ["meds"],
-    }
   ],
 }
--- src/app/models/diary.store.spec.ts
+++ src/app/models/diary.store.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from "@angular/core/testing"
 import { DiaryStore } from "./diary.store"
+import { DiaryEntry } from "./diary-entry.model";
 
 describe('DiaryStore', () => {
   it("should add entries", () => {
     const store = TestBed.inject(DiaryStore);
-    expect(1 + 1).toBe(2);
+    let entry: DiaryEntry = {
+      id: undefined,
+      date: "2025-01-01",
+      stomach: 2,
+      sleepQuality: 2
+    }
+    store.addEntry(entry);
+    expect(store.diaryEntries()).toHaveLength(1);
   });
 });
